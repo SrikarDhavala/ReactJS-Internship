0811f5af3176f0588b0b088c791386f754bac871
SrikarDhavala <srikardhavala5305@gmail.com>
2025-05-23 19:41:59 +0530

    Created a currency converter app

---
+++ currency-converter/src/App.jsx
@@ -0,0 +1,45 @@
+
+function App() {
+  return(
+    <div className="currency-converter">
+      <h2 className="title">Currency Converter</h2>
+      <form className="converter-form">
+        <div className="form-group">
+          <label className="form-label">Enter Amount</label>
+          <input type="number" className="form-input" required />
+        </div>
+        <div className="form-group form-currency-group">
+          <div className="form-section">
+            <label className="form-label">From</label>
+            <div className="currency-select">
+              <img src="https://flagsapi.com/US/flat/64.png" alt="Flag" />
+              <select className="currency-dropdown">
+                <option value="USD" selected>USD</option>
+                <option value="INR">INR</option>
+                <option value="NPR">NPR</option>
+              </select>
+            </div>
+          </div>
+
+          <div className="swap-icon">⇌</div>
+
+          <div className="form-section">
+            <label className="form-label">To</label>
+            <div className="currency-select">
+              <img src="https://flagsapi.com/IN/flat/64.png" alt="Flag" />
+              <select className="currency-dropdown">
+                <option value="USD">USD</option>
+                <option value="INR" selected>INR</option>
+                <option value="NPR">NPR</option>
+              </select>
+            </div>
+          </div>
+        </div>
+        <button type="submit" className="submit-button">Get Exchange Rate</button>
+        <p className="exchange-rate-result">1,000 USD = 83620.80 INR</p>
+      </form>
+    </div>
+  );
+}
+
+export default App
