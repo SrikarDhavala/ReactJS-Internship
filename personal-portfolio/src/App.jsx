383d46a6ca23ffd38af7d223e9de5e661078691b
SrikarDhavala <srikardhavala5305@gmail.com>
2025-06-02 00:49:06 +0530

    Added data and styling into Home and About sections

--- personal-portfolio/src/App.jsx
+++ personal-portfolio/src/App.jsx
@@ -3,16 +3,23 @@ import './App.css'
 import LoadingScreen from './components/LoadingScreen'
 import './index.css'
 import NavBar from './components/NavBar';
+import MobileMenu from './components/MobileMenu';
+import Home from './components/sections/Home';
+import About from './components/sections/About';
 
 function App() {
 
   const [isLoaded, setIsLoaded] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
   
   return(
     <>
-      {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)}/>}{""}
+      {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)}/>}
       <div className={`min-h-screen transition-opacity duration-700 ${isLoaded ? "opacity-100" : "opacity-0"} bg-black text-gray-100`}>
-        <NavBar />
+        <NavBar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+        <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+        <Home />
+        <About />
       </div>
     </>
   );
