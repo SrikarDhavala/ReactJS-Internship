b721d816cc6bef6918d852e157af4d34f29ad95a
SrikarDhavala <srikardhavala5305@gmail.com>
2025-06-07 14:04:24 +0530

    Added Contact and Footer sections

--- advanced-portfolio/src/components/Contact.jsx
+++ advanced-portfolio/src/components/Contact.jsx
@@ -2,7 +2,30 @@
 function Contact() {
 
     return(
-        <></>
+        <section id="contact" className='flex flex-col items-center justify-center py-24 px-[12vw] md:px-[7vw] lg:px-[20vw]'>
+            <div className='text-center mb-16'>
+                <h2 className='text-4xl sm:text-4xl font-bold text-white'>Contact</h2>
+                <div className='w-32 h-1 bg-blue-500 mx-auto mt-2'></div>
+                <p className='text-gray-400 mt-4 text-lg font-semibold'>
+                    I'd love to hear from you - reach out for any opportunities or questions!
+                </p>
+            </div>
+
+            <div className="mt-8 w-full max-w-md bg-[#030a1f] p-6 rounded-lg shadow-lg border border-gray-700">
+                <h3 className="text-xl font-semibold text-white text-center">
+                    Connect With Me
+                </h3>
+
+                <form className="mt-4 flex flex-col space-y-4">
+                    <input type="email" name="user_email" placeholder="Your Email" required className="w-full p-3 rounded-md bg-[#040e29] text-white border border-gray-600 focus:outline-none focus:border-blue-500" />
+                    <input type="text" name="user_name" placeholder="Your Name" required className="w-full p-3 rounded-md bg-[#040e29] text-white border border-gray-600 focus:outline-none focus:border-blue-500" />
+                    <input type="text" name="subject" placeholder="Subject" required className="w-full p-3 rounded-md bg-[#040e29] text-white border border-gray-600 focus:outline-none focus:border-blue-500" />
+                    <textarea name="message" placeholder="Message" rows="4" className="w-full p-3 rounded-md bg-[#040e29] text-white border border-gray-600 focus:outline-none focus:border-blue-500"></textarea>
+
+                    <button type="submit" className="w-full bg-gradient-to-r from-blue-800 to-blue-500 py-3 text-white font-semibold rounded-md hover:opacity-90 transition cursor-pointer">Send</button>
+                </form>
+            </div>
+        </section>
     );
 }
 
--- advanced-portfolio/src/components/Footer.jsx
+++ advanced-portfolio/src/components/Footer.jsx
@@ -1,8 +1,52 @@
+import { FaLinkedin, FaGithub, FaInstagram, FaYoutube, FaTwitter } from "react-icons/fa";
 
 function Footer() {
 
-    return(
-        <></>
+    const handleScroll = (sectionId) => {
+        const sectionElement = document.getElementById(sectionId);
+        if (sectionElement) {
+            sectionElement.scrollIntoView({ behavior: "smooth" });
+        }
+    }
+
+    return (
+        <footer className="text-white py-8 px-[12vw] md:px-[7vw] lg:px-[20vw]">
+            <div className="container mx-auto text-center">
+                <h2 className="text-xl font-semibold text-blue-500">
+                    Srikar
+                </h2>
+
+                <nav className="flex flex-wrap justify-center space-x-4 sm:space-x-6 mt-2">
+                    {[
+                        { name: "About", id: "about" },
+                        { name: "Skills", id: "skills" },
+                        { name: "Experience", id: "experience" },
+                        { name: "Projects", id: "projects" },
+                        { name: "Education", id: "education" },
+                    ].map((item, index) => (
+                        <button key={index} onClick={() => handleScroll(item.id)} className="hover:text-blue-500 text-sm sm:text-base my-1 cursor-pointer">{item.name}</button>
+                    ))}
+                </nav>
+
+                <div className="flex flex-wrap justify-center space-x-4 mt-6">
+                    {[
+                        {icon: <FaGithub />, link: "#"},
+                        {icon: <FaLinkedin />, link: "#"},
+                        {icon: <FaInstagram />, link: "#"},
+                        {icon: <FaYoutube />, link: "#"},
+                        {icon: <FaTwitter />, link: "#"},
+                    ].map((social, index) => (
+                        <a key={index} href={social.link} target="_blank" rel="noopener noreferrer" className="text-xl hover:text-blue-500 transition-transform transform hover:scale-140">
+                            {social.icon}
+                        </a>
+                    ))}
+                </div>
+
+                <p className="text-sm text-gray-400 mt-2">
+                    &copy; 2025 Srikar. All rights are not reserved, but please dont use it ^_^
+                </p>
+            </div>
+        </footer>
     );
 }
 
